642d0c3bd3da3edd57cbeaea568cfe1db46fe284
edraizen <edraizen@gmail.com>
2015-05-26 19:56:13 -0400

    named urls

--- HistoneDB/urls.py
+++ HistoneDB/urls.py
@@ -7,13 +7,13 @@ from django.conf import settings
 urlpatterns = patterns('browse.views',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'browse_types'),
-    url(r'^browse/$', 'browse_types', name="browse_types"),
+    url(r'^browse/$', 'browse_types', namespace="browse_types"),
 
-    url(r'^type/([a-zA-Z0-9]+)/$', 'browse_variants', name="browse_variants"),
-    url(r'^variant/([a-zA-Z0-9\.]+)/$', 'browse_variant', name="browse_variant"),
+    url(r'^type/([a-zA-Z0-9]+)/$', 'browse_variants', namespace="browse_variants"),
+    url(r'^variant/([a-zA-Z0-9\.]+)/$', 'browse_variant', namespace="browse_variant"),
 
-    url(r'^search/$', 'search', name="search"),
-    url(r'^upload/$', 'upload', name="upload"),
+    url(r'^search/$', 'search', namespace="search"),
+    url(r'^upload/$', 'upload', namespace="upload"),
 
     #url(r'^data/type/phyloxml/([a-zA-Z0-9]+)/', 'get_phyloxml_of_type'),
     url(r'^data/(type)/json/([a-zA-Z0-9]+)/species/$', 'get_starburst_json'),
