bac634f33a5491492848670bfac95f086eec3b7d
edraizen <edraizen@gmail.com>
2015-05-26 18:30:29 -0400

    named urls

--- HistoneDB/urls.py
+++ HistoneDB/urls.py
@@ -7,15 +7,15 @@ from django.conf import settings
 urlpatterns = patterns('browse.views',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'browse_types'),
-    url(r'^browse/$', 'browse_types'),
+    url(r'^browse/$', 'browse_types', name="browse_types"),
 
-    url(r'^type/([a-zA-Z0-9]+)/$', 'browse_variants'),
-    url(r'^variant/([a-zA-Z0-9\.]+)/$', 'browse_variant'),
+    url(r'^type/([a-zA-Z0-9]+)/$', 'browse_variants', name="browse_variants"),
+    url(r'^variant/([a-zA-Z0-9\.]+)/$', 'browse_variant', name="browse_variant"),
 
-    url(r'^search/$', 'search'),
-    url(r'^upload/$', 'upload'),
+    url(r'^search/$', 'search', name="search"),
+    url(r'^upload/$', 'upload', name="upload"),
 
-    url(r'^data/type/phyloxml/([a-zA-Z0-9]+)/', 'get_phyloxml_of_type'),
+    #url(r'^data/type/phyloxml/([a-zA-Z0-9]+)/', 'get_phyloxml_of_type'),
     url(r'^data/(type)/json/([a-zA-Z0-9]+)/species/$', 'get_starburst_json'),
     url(r'^data/(type)/json/([a-zA-Z0-9]+)/all/$', 'get_sequence_table_data'),
 
