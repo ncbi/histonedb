797429bca5ae2249257f7c786d2abd68671537d7
Eli Draizen <edraizen@gmail.com>
2015-05-28 11:21:35 -0400

    Strat tree building command

---
+++ browse/management/commands/buildtrees.py
@@ -0,0 +1,82 @@
+from django.core.management.base import BaseCommand, CommandError
+from browse.models import *
+from tools.load_hmmsearch import parseHmmer
+from tools.test_model import test_model
+import subprocess
+import os, sys
+from Bio import Phylo
+from Bio.Phylo import PhyloXMLIO
+
+class Command(BaseCommand):
+    help = 'Build the HistoneDB by training HMMs with seed sequences found in seeds directory in the top directory of thi project and using those HMMs to search the NR database.'
+    seed_directory = os.path.join("static", "browse", "seeds")
+    hmm_directory = os.path.join("static", "browse", "hmms")
+    combined_varaints_file = os.path.join(hmm_directory, "combined_variants.hmm")
+    pressed_combined_varaints_file = os.path.join(hmm_directory, "combined_variants.h3f")
+    results_file = "{}.out".format(combined_varaints_file)
+    nr_file = "nr"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-f", 
+            "--force", 
+            default=False, 
+            action="store_true", 
+            help="Force the recreation of the HistoneDB. If True and an hmmsearch file is not present, the program will redownload the nr db if not present, build and press the HMM, and search the nr databse using the HMMs.")
+
+        parser.add_argument(
+            "--nr",
+            default=False,
+            action="store_true",
+            help="Redownload NR database. If force is also True, this option is redundant")
+
+    def handle(self, *args, **options):
+        self.make_trees()
+        self.convert_to_phyloxml()
+        self.add_features()
+
+    def get_seeds(self):
+        for i, (root, _, files) in enumerate(os.walk(self.seed_directory)):
+            core_type = os.path.basename(root)
+            if i==0:
+                #Skip parent directory, only allow variant hmms to be built/searched
+                continue
+            for seed in files: 
+                if not seed.endswith(".fasta"): continue
+                yield core_type, seed
+
+    def make_trees(self):
+        for i, (root, _, files) in enumerate(os.walk(self.seed_directory)):
+            core_type = os.path.basename(root)
+            if i==0:
+                #Skip parent directory, only allow variant hmms to be built/searched
+                continue
+            combined_seed_file = os.path.join(self.trees_path, "{}.fasta".format(core_histone))
+            combined_seed_aligned = os.path.join(self.trees_path, "{}_aligned.fasta".format(core_histone))
+            with open(combined_seed_file, "w") as combined_seed:
+                for seed in files: 
+                    if not seed.endswith(".fasta"): continue
+                    for s in SeqIO.parse(os.path.join(self.seed_directory, core_type, seed), "fasta")
+                        s.seq = s.seq.ungap("-")
+                        SeqIO.write(s, combined_seed, "fasta")
+
+            tree = os.path.join(self.trees_path, "{}.nwk".format(core_histone))
+            subprocess.call(["muscle", "-in", combined_seed, '-out', combined_seed_aligned])
+            subprocess.call(["clustalw2", "-infile", combined_seed_aligned, '-tree', '-out', tree])
+            Phylo.convert(tree, 'newick',
+                          os.path.join(self.trees_path, "{}.xml".format(core_histone)), 'phyloxml')
+    
+    def add_features(self):
+        for core_histone in ["H2A", "H2B", "H3", "H4", "H1"]:
+            phx = PhyloXMLIO.read(os.path.join(self.trees_path, "{}.xml".format(core_histone)), 'phyloxml')
+            for phylogeny in tree.phylogenies:
+                for variant in Variant.objects.filter(core_type__id=core_histone):
+                    '<{0} fill="#000" stroke="#000" opacity="0.7" label="{0}" labelStyle="sectorHighlightText" />'.format(variant.id)
+                for clade in phylogeny:
+                    genus, gi, variant = clade.name.split("|")
+                    clade.name = "{}_{}".format(genus, gi)
+                    "<chart><group>{}</group></chart>".format(variant)
+            PhyloXMLIO.write(phx, 'ex_no_other.xml')
+
+
+
