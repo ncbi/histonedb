a04ab6915b756b8457f1a24f6b8b4eddeda8a6d7
Eli Draizen <edraizen@gmail.com>
2015-05-28 17:16:29 -0400

    Start working on simple build to build everything

--- browse/management/commands/build.py
+++ browse/management/commands/build.py
@@ -11,40 +11,8 @@ class Command(BaseCommand):
         parser.add_argument('-v', '--verbose' help="Print info")
 
     def handle(self, *args, **options):
-        #Download nr
-        nr_file = "nr"
-        if not os.isfile(nr_file):
-            process = Popen(["curl", "-#", "ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nr.gz", ">", "nr.gz"])
-            process.communicate()
-            process = Popen(["tar", "-xf", "nr.gz"])
-            os.remove("nr.gz")
-
-        #Build and search HMMs
-        seed_directory = os.path.join("static", "browse", "seeds")
-        hmm_directory = os.path.join("static", "browse", "hmms", options["core_type"])
-        combined_varaints_file = os.path.join(hmm_directory, "combined_variants.hmm")
-        with open(combined_varaints_file) as combined_variants:
-            for root, _, files in os.walk(seed_directory):
-                core_type = os.path.basename(root)
-                if not core_type:
-                    #Skip parent directory, only allow variant hmms to be built/searched
-                    continue
-                for seed in files:
-                    if not seed.endswith(".fasta"): continue
-                    hmm_file = os.path.join(hmm_directory, "{}.hmm".format(seed.split("_")[0]))
-                    process = Popen(["hmmbuild", hmm, os.path.join("seeds", seed)])
-                    process.communicate()
-                    with open(hmm_file) as hmm:
-                        print >> combined_variants, hmm.read().rstrip()
-
-        #Press the HMMs
-        process = Popen(["hmmpress", combined_varaints_file])
-        process.communicate()
-
-        #Use HMMs to search the nr database
-        results_file = os.path(hmm_directory, "{}.out".format(options["core_type"]))
-        process = Popen(["hmmsearch", "-o", results_file, "--cpu", "4", "--notextw", "--incdomE", "0.1", combined_varaints_file, nr_file])
-        process.communicate()
-
-        #Load data into the histone database
-        load_hmmsearch(results_file, nr_file)
+        """
+        1. Build Variants
+        2. Build Trees
+        3. Build Sunbursts
+        """
