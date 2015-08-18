a0ce4154463e0b0d126f5a5f19cbb269b1099262
Alexey Shaytan <shaytanak@gmail.com>
2015-08-18 12:43:49 -0400

    Started aux_tools scripts folder

---
+++ aux_tools/README.md
@@ -0,0 +1,2 @@
+Helper tools and scripts,
+to create and administer the seed alignments and annotation of histones.
---
+++ aux_tools/gis_to_align.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Gets a file with GIs, obtains FASTA from NCBI and aligns with MUSCLE.
+"""
+
+from math import sqrt, log, e
+from random import choice, random
+from Utils import indices
+
+
+class NumberList(list):
+    pass    #much code deleted
+class FrequencyDistribution(dict):
+    pass    #much code deleted
+
+if __name__ == '__main__':    #code to execute if called from command-line
+    pass    #do nothing - code deleted
+#use this either for a simple example of how to use the module,
+#or when the module can meaningfully be called as a script.
