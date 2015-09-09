fa382cbbfd75bc53284b6b4b5a01711b4f1cff6b
Alexey Shaytan <alexeyshaytan@gmail.com>
2015-09-09 00:51:03 -0400

    Fixed quickmsa sequnce

--- browse/views.py
+++ browse/views.py
@@ -148,7 +148,7 @@ def browse_variant(request, histone_type, variant,gi):
     if not human_sequence:
         human_sequence = sequences.filter(taxonomy__name="homo sapiens").first()
     if not human_sequence:
-        human_sequence = sequences.first()
+        human_sequence = sequences.first(reviewed=True)
     print human_sequence
 
     try:
