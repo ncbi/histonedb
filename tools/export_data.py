7889cc9e6e61c8c0700b895cb9b27c54b2d2fe62
Alexey Shaytan <alexeyshaytan@gmail.com>
2016-01-12 16:52:33 -0500

    Updates to help.

--- tools/export_data.py
+++ tools/export_data.py
@@ -9,7 +9,7 @@ with open('seqs.txt','w') as f:
         f.write("%s,%s,%s,%s,%s\n"%(seq.gi,seq.variant.hist_type,seq.variant,seq.taxonomy_id,seq.reviewed))
 
 with open('scores.txt','w') as f:
-    f.write("gi,hist_var,score,used_for_classification\n")
+    f.write("gi,hmm_model,score,used_for_classification\n")
     for s in Score.objects.all():
         f.write("%s,%s,%s,%s\n"%(s.sequence.gi,s.variant,s.score,s.used_for_classification))
 
