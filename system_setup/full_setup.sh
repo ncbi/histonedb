fb9c49086fbea882e9d8c63047238cfd280a48ef
l-singh-biomsu <l.singh@intbio.org>
2019-12-02 14:16:39 +0300

    full_setup changed

--- system_setup/full_setup.sh
+++ system_setup/full_setup.sh
@@ -4,6 +4,8 @@ yes Y | sh system_setup/sys_requirements_setup.txt
 yes Y | sh system_setup/py_requirements_setup.txt
 # Create DB
 sudo mysql -u root --skip-password < system_setup/db_setup_query.sql
+sudo sed -i '$a wait_timeout = 31536000' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed -i '$a interactive_timeout = 31536000' /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # To cinfigure apache2 with mod_wsgi (already installed) please, see
 # https://medium.com/faun/how-to-set-up-conda-virtual-environments-with-apache-mod-wsgi-flask-c2043711223e
