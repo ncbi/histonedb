26d06d829c09324af5b044027b6fc4e9d60c8b5d
l-singh-biomsu <l.singh@intbio.org>
2020-02-03 16:29:22 +0300

    Docker building

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-db_create=false
-reinit_histdb=false
+#db_create=false
+#reinit_histdb=false
+#
+#while [ -n "$1" ]
+#do
+#case "$1" in
+#-db) db_create=true ;;
+#-re) reinit_histdb=true ;;
+#*) echo "$1 is not an option" ;;
+#esac
+#shift
+#done
 
-while [ -n "$1" ]
-do
-case "$1" in
--db) db_create=true ;;
--re) reinit_histdb=true ;;
-*) echo "$1 is not an option" ;;
-esac
-shift
-done
-
-if [ -d /var/lib/mysql/histonedb || $db_create ] ; then
-    # Do Stuff ...
+if [ -d /var/lib/mysql/histonedb ] ; then
+  echo "Running the existing database localed at /var/lib/mysql/histonedb inside container."
+else
+  cd /var/www/histonedb
+  echo 'Database creating ...'
+  sudo mysql -u root --skip-password < system_setup/db_setup_query.sql
 fi
--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ sh system_setup/full_setup.sh
 echo 'Apache2 and WSGI configurating ...'
 sh system_setup/apache_setup/apache_setup.sh
 
-echo 'DataBase creating ...'
-sudo mysql -u root --skip-password < /system_setup/db_setup_query.sql
+echo 'Database creating ...'
+sudo mysql -u root --skip-password < system_setup/db_setup_query.sql
 
 echo 'Start initialization ...'
 sh reinit_histdb_local.sh
