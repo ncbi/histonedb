8ad23152ad0a1e37272011c0847ce37c2cb41188
Alex Sha <alex@intbio.org>
2020-03-04 12:30:47 +0300

    Wed Mar  4 12:30:47 MSK 2020

--- reinit_histdb_local.sh
+++ reinit_histdb_local.sh
@@ -15,7 +15,7 @@ python manage.py migrate
 python manage.py buildncbi_fast
 python manage.py loadtaxonomy
 python manage.py buildtaxonomytoc
-python manage.py buildvariants -f
+python manage.py buildvariants_parallel -f
 # python manage.py buildvariants -f --db yeast.aa
 # python manage.py buildvariants -f --db swissprot
 python manage.py buildtrees -f
