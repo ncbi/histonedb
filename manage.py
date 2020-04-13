e2cbe3331a1ef955e74cdc8d633e298ad628ae15
СиÐыSingh Lovepreet <l_singh@newton.bioeng.ru>
2020-04-13 19:44:21 +0300

    System setup for py36, added virtualenv

--- manage.py
+++ manage.py
@@ -5,6 +5,15 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HistoneDB.settings")
 
+#    import sys
+#    output = ''
+#    output += 'sys.version = %s\n' % repr(sys.version)
+#    output += 'sys.prefix = %s\n' % repr(sys.prefix)
+#    output += 'sys.path = %s' % repr(sys.path)
+#    print('------------------------------')
+#    print(output)
+#    import django
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
